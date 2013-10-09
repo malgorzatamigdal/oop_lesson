86d42e4317915dc4d5032432ebca40224a088ca9
Hackbright Student <user@chromebox-002.(none)>
2013-10-08 17:00:51 -0700

    Added conditional opening of chest, conditional key appearance and smart score counting

--- game.py
+++ game.py
@@ -53,11 +53,23 @@ class Gem(GameElement):
     IMAGE = "BlueGem"
     SOLID = False
     def interact(self,player):
+        global score
         player.inventory.append(self)
+        score += 15
+        GAME_BOARD.draw_msg("You just acquired a blue gem for 15 points! Your score is: %s"% (score))
+        if score > 150:
+            place_key()
 
 class GreenGem(Gem):
     IMAGE = "GreenGem"
     SOLID = False
+    def interact(self,player):
+        global score
+        player.inventory.append(self)
+        score += 30
+        GAME_BOARD.draw_msg("You just acquired a green gem for 30 points! Your score is: %s"% (score))
+        if score > 150:
+            place_key()
 
 class Enemy(GameElement):
     IMAGE = "Enemy"
@@ -69,16 +81,46 @@ class Tree(GameElement):
         hugged = 0
         player.inventory.append(self)
         hugged = hugged +1
-        message = "Very well, tree hugger! This tree has been hugged %s times" %hugged
+        message = "Hug the tree!"
         GAME_BOARD.draw_msg(message)
  
-
 class Chest(GameElement):
     contents = ""
     IMAGE = "Chest"
     SOLID = True
     def interact(self, player):
+        global score
+
         GAME_BOARD.draw_msg("You just opened the chest")
+        contents = ["gem", "gross garbage"]
+        item = random.choice(contents)
+
+        if item == "gem":
+            gem_in_chest = GreenGem()
+            GAME_BOARD.del_el(self.x, self.y)
+            GAME_BOARD.register(gem_in_chest)
+            GAME_BOARD.set_el(self.x, self.y, gem_in_chest)
+            score += 30
+            GAME_BOARD.draw_msg("Yay a gem! Your score is: %s"% (score))
+        else:
+            gross_garbage = Enemy()
+            GAME_BOARD.del_el(self.x, self.y)
+            GAME_BOARD.register(gross_garbage)
+            GAME_BOARD.set_el(self.x, self.y, gross_garbage)  
+            score -= 20
+            GAME_BOARD.draw_msg("Unfortunately, you found an enemy bug...You lost 20 points. Your score is: %s"% (score))
+        if score > 150:
+            place_key()
+        
+
+class Key(GameElement):
+    IMAGE = "Key"
+    SOLID = False
+    def interact(self,player):
+        global score
+        player.inventory.append(self)
+        GAME_BOARD.draw_msg("You have just bought a key for 100 points.")
+        score -= 150
         
 #### Put class definitions here ####
 
@@ -131,11 +173,40 @@ def initialize():
         if not GAME_BOARD.get_el(random_x, random_y):
             GAME_BOARD.register(random_gems[i])
             GAME_BOARD.set_el(random_x, random_y, random_gems[i])
+
+    for i in range(0,6):
+        random_rocks = [Rock()] * 6
+        random_rock_x = random.choice(range(0,GAME_WIDTH))
+        random_rock_y = random.choice(range(0, GAME_HEIGHT))
+        if not GAME_BOARD.get_el(random_rock_x, random_rock_y):
+            GAME_BOARD.register(random_rocks[i])
+            GAME_BOARD.set_el(random_rock_x, random_rock_y, random_rocks[i])
+
+    for i in range(0,3):
+        random_tree = [Tree()] * 3
+        random_tree_x = random.choice(range(0,GAME_WIDTH))
+        random_tree_y = random.choice(range(0, GAME_HEIGHT))
+        if not GAME_BOARD.get_el(random_tree_x, random_tree_y):
+            GAME_BOARD.register(random_tree[i])
+            GAME_BOARD.set_el(random_tree_x, random_tree_y, random_tree[i])           
    
-   
+def place_key():
+        GAME_BOARD.draw_msg("Congratulations! You have just revealed a key.")
+        random_key = Key()
+        appeared = 0
+        while not appeared:
+            key_x = random.choice(range(0,GAME_WIDTH))
+            key_y = random.choice(range(0,GAME_HEIGHT))
+            if not GAME_BOARD.get_el(key_x, key_y):
+                GAME_BOARD.register(random_key)
+                GAME_BOARD.set_el(key_x, key_y, random_key)
+                appeared = 1
+
 def keyboard_handler():
-    direction = None
+
     global score
+    direction = None
+  
     if  KEYBOARD[key.SPACE] and KEYBOARD[key.UP]:
         direction = "jump_up"
     elif KEYBOARD[key.UP]:
@@ -152,11 +223,10 @@ def keyboard_handler():
         direction = "jump_right"
     elif KEYBOARD[key.RIGHT]:
         direction = "right"
-    #elif KEYBOARD[key.S]:
-        #GAME_BOARD.draw_msg("Your score is: %s"%score)
 
     if direction:
         next_location = PLAYER.next_pos(direction)
+
         if next_location[0] not in range(0, GAME_WIDTH) or next_location[1] not in range(0, GAME_HEIGHT):
             next_x = PLAYER.x
             next_y = PLAYER.y
@@ -164,33 +234,19 @@ def keyboard_handler():
             next_x = next_location[0]
             next_y = next_location[1]
         existing_el = GAME_BOARD.get_el(next_x, next_y)
-        if existing_el:
-            existing_el.interact(PLAYER)
-            if isinstance(existing_el, GreenGem):
-                score += 30
-                GAME_BOARD.draw_msg("You just acquired a green gem for 30 points! Your score is: %s"% (score))
-            elif isinstance(existing_el, Gem):
-                score += 15
-                GAME_BOARD.draw_msg("You just acquired a blue gem for 15 points! Your score is: %s"% (score))
-            elif isinstance(existing_el, Chest):
-                contents = ["gem", "gross garbage"]
-                item = random.choice(contents)
-                if item == "gem":
-                    gem_in_chest = GreenGem()
-                    GAME_BOARD.del_el(existing_el.x, existing_el.y)
-                    GAME_BOARD.register(gem_in_chest)
-                    GAME_BOARD.set_el(existing_el.x, existing_el.y, gem_in_chest)
-                    score += 5
-                    GAME_BOARD.draw_msg("Yay a gem! Your score is: %s"% (score))
-                else:
-                    gross_garbage = Enemy()
-                    GAME_BOARD.del_el(existing_el.x, existing_el.y)
-                    GAME_BOARD.register(gross_garbage)
-                    GAME_BOARD.set_el(existing_el.x, existing_el.y, gross_garbage)  
-                    score -= 20
-                    GAME_BOARD.draw_msg("Unfortunately, you found an enemy bug...You lost 20 points. Your score is: %s"% (score))
-                
 
+        if existing_el:
+            if isinstance(existing_el, Chest):
+                key_yes = 0
+                for i in PLAYER.inventory:
+                    if isinstance(i, Key):
+                        key_yes = 1
+                        existing_el.interact(PLAYER)
+                        break
+                if key_yes != 1:
+                    GAME_BOARD.draw_msg("Sorry, you can't open the chest unless you have a key...")
+            else:
+                existing_el.interact(PLAYER)
                 
         if existing_el is None or not existing_el.SOLID:
             GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
